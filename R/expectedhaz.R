b350822c21cd93a195b76129982fe5accad8dae0
Thomas Ollard <thomas.ollard@univ-poitiers.fr>
2026-06-25 15:33:01 +0200

    Update expectedhaz.R

--- R/expectedhaz.R
+++ R/expectedhaz.R
@@ -4,18 +4,22 @@
 
 expectedhaz <- function(ratetable, age, year, sex, time, max_age = NULL, max_year = NULL) 
 {
-  time <- min(time, 1000000)
+  time <- pmin(time, 1000000)
   .year <- as.numeric(format( as.Date(time + year, origin = "1960-01-01"), "%Y" ) ) 
+  .age <- round((age+time)/365.24)
+  if(is.null(max_age)){
+    max_age <- max(as.numeric(dimnames(ratetable)[[1]]))
+  }
   
-  .age <- round((age+time)/365.24) 
-  if(is.null(max_age)){ max_age <- max(as.numeric(dimnames(ratetable)[[1]]))}
+  if(is.null(max_year)){
+    max_year <- max(as.numeric(dimnames(ratetable)[[2]]))
+  }
   
-  if(is.null(max_year)){max_year <- max(as.numeric(dimnames(ratetable)[[2]]))}
-  
-  idx <- cbind(as.character(pmin(.age,  max_age)), 
-               as.character(pmin(.year, max_year)),
-               sex)
+  idx <- cbind(
+    as.character(pmin(.age,  max_age)),
+    as.character(pmin(.year, max_year)),
+    sex
+  )
   
   ratetable[idx]
 }
-  
